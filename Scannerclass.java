29834b50af438e08cd270354da21c205c84b4a69
Sowmya2343 <sowmya12chada@gmail.com>
2025-07-26 07:25:14 +0530

    Add files via upload

---
+++ Scannerclass.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Scannerclass {
+	public static void main(String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the  byte data:");
+		byte b=sc.nextByte();
+		System.out.println("The byte data:"+b);
+		System.out.println("--------------------");
+		System.out.println("Enter the  short data:");
+		short s=sc.nextShort();
+		System.out.println("The short data:"+s);
+		System.out.println("--------------------");
+		System.out.println("Enter the  integer data:");
+		int i=sc.nextInt();
+		System.out.println("The integer data:"+i);
+		System.out.println("--------------------");
+		System.out.println("Enter the  long data:");
+		long l=sc.nextLong();
+		System.out.println("The Long data:"+l);
+		System.out.println("--------------------");
+		System.out.println("Enter the  float data:");
+		float f=sc.nextFloat();
+		System.out.println("The float data:"+f);
+		System.out.println("--------------------");
+		System.out.println("Enter the  double data:");
+		double d=sc.nextDouble();
+		System.out.println("The double data:"+d);
+		System.out.println("--------------------");
+		sc.nextLine();
+		System.out.println("Enter the  String data:");
+		String str=sc.nextLine();
+		System.out.println("The string data:"+str);
+		System.out.println("--------------------");
+        sc.close();
+	
+		
+	}
+
+}
